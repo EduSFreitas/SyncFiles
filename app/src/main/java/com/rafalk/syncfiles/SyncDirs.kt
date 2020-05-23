84d1f85d73d14757852d973692b0461c55404e10
rafalk342 <rafalk342@gmail.com>
2020-05-23 14:26:39 +0200

    Update remote/local files if modified

--- app/src/main/java/com/rafalk/syncfiles/SyncDirs.kt
+++ app/src/main/java/com/rafalk/syncfiles/SyncDirs.kt
@@ -4,7 +4,10 @@ import android.webkit.MimeTypeMap
 import com.google.api.client.http.FileContent
 import com.google.api.client.util.DateTime
 import com.google.api.services.drive.Drive
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.io.File
 import java.io.FileOutputStream
@@ -42,12 +45,12 @@ class SyncDirs(
     }
 
     private fun showDifference() {
-        val common = ArrayList<String>()
+        val common = HashSet<String>()
         for (entry in mapOfDriveFiles) {
             if (!mapOfLocalFiles.containsKey(entry.key)) {
                 Timber.d("Local ${entry.key} is missing")
                 downloadFile(entry.key, entry.value)
-            } else{
+            } else {
                 common.add(entry.key)
             }
         }
@@ -55,27 +58,70 @@ class SyncDirs(
         for (entry in mapOfLocalFiles) {
             if (!mapOfDriveFiles.containsKey(entry.key)) {
                 Timber.d("Remote ${entry.key} is missing")
-            } else{
+                uploadFile(entry.key, entry.value)
+            } else {
                 common.add(entry.key)
             }
+        }
+
+        for (name in common) {
+            val localLastModified = mapOfLocalFiles.getValue(name).lastModified()
+            val remoteLastModified = mapOfDriveFiles.getValue(name).modifiedTime.value
+            when {
+                localLastModified > remoteLastModified -> {
+                    Timber.d("Local file ${name} is newer")
+                    updateFile(mapOfLocalFiles.getValue(name), mapOfDriveFiles.getValue(name).id)
+                }
+                localLastModified < remoteLastModified -> {
+                    Timber.d("Drive file ${name} is newer")
+                    downloadFile(name, mapOfDriveFiles.getValue(name))
+                }
+                else -> {
+                    Timber.d("File ${name} is up to date")
+                }
+            }
+        }
+    }
+
+    private fun updateFile(file: File, id: String) {
+        val driveFile = com.google.api.services.drive.model.File()
+        val driveContent = FileContent(
+            MimeTypeMap.getSingleton().getMimeTypeFromExtension(file.extension),
+            file
+        )
+        driveFile.modifiedTime = DateTime(file.lastModified())
+        launch(Dispatchers.Default) {
+            googleDriveService.files().update(id, driveFile, driveContent).execute()
+            Timber.d("${file.name} updated")
+        }
+    }
+
+    private fun uploadFile(name: String, file: File) {
+        val driveFile = com.google.api.services.drive.model.File()
+        val driveContent = FileContent(
+            MimeTypeMap.getSingleton().getMimeTypeFromExtension(file.extension),
+            file
+        )
+        driveFile.name = name
+        driveFile.parents = mutableListOf(driveDirId)
+        driveFile.modifiedTime = DateTime(file.lastModified())
+        launch(Dispatchers.Default) {
+            googleDriveService.files().create(driveFile, driveContent).execute()
+            Timber.d("${file.name} uploaded")
         }
     }
 
     private fun downloadFile(name: String, file: com.google.api.services.drive.model.File) =
         launch(Dispatchers.Default) {
             val localFile = File(localDir + '/' + name)
-            localFile.setLastModified(file.modifiedTime.value)
-            if (!localFile.exists()) {
-                localFile.createNewFile()
-            }
-
             val localOutputStream = FileOutputStream(localFile)
             googleDriveService.files().get(file.id)
                 .executeMediaAndDownloadTo(localOutputStream)
             localOutputStream.flush()
             localOutputStream.close()
+            localFile.setLastModified(file.modifiedTime.value)
 
-            Timber.d("${localFile.absolutePath} saved to local storage")
+            Timber.d("${localFile.absolutePath} saved to local storage ${DateTime(localFile.lastModified())}")
         }
 
     private fun getMapOfLocalFiles(localDir: String): Map<String, File> {
@@ -83,58 +129,7 @@ class SyncDirs(
         val localFiles = local.listFiles { file ->
             file.isFile
         }
-        Timber.d(localFiles.toString())
+        Timber.d(localFiles.contentToString())
         return localFiles.map { it.name to it }.toMap()
-    }
-
-    private fun local() {
-        val local = File(localDir)
-        val localFiles = local.listFiles { file ->
-            file.isFile
-        }
-        for (file in localFiles) {
-            Timber.d("Local ${file.name}")
-            val driveFile = com.google.api.services.drive.model.File()
-            val driveContent = FileContent(
-                MimeTypeMap.getSingleton().getMimeTypeFromExtension(file.extension),
-                file
-            )
-            driveFile.name = file.name
-            driveFile.parents = mutableListOf(driveDirId)
-            driveFile.modifiedTime = DateTime(file.lastModified())
-            launch(Dispatchers.Default) {
-                googleDriveService.files().create(driveFile, driveContent).execute()
-                Timber.d("${file.name} uploaded")
-            }
-
-        }
-    }
-
-    private fun drive() {
-        launch(Dispatchers.Default) {
-            val driveFiles = googleDriveService
-                .files().list()
-                .setSpaces("drive")
-                .setQ("'${driveDirId}' in parents and trashed=false")
-                .setFields("nextPageToken, files(id, name, mimeType, modifiedTime)")
-                .setPageToken(null)
-                .execute()
-            Timber.d("Result received $driveFiles")
-            for (file in driveFiles.files) {
-                val localFile = File(localDir + '/' + file.name)
-                localFile.setLastModified(file.modifiedTime.value)
-                if (!localFile.exists()) {
-                    localFile.createNewFile()
-                }
-
-                val localOutputStream = FileOutputStream(localFile)
-                googleDriveService.files().get(file.id)
-                    .executeMediaAndDownloadTo(localOutputStream)
-                localOutputStream.flush()
-                localOutputStream.close()
-
-                Timber.d("${localFile.absolutePath} saved to local storage")
-            }
-        }
     }
 }
